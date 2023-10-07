389638e0f7e86ff88ebef997e6a8db6fe599ddcc
Abhi <Abhi@DELL>
2023-10-07 10:37:06 +0530

    commit

---
+++ assignment/src/org/tnsif/assignment/Bero.java
@@ -0,0 +1,36 @@
+package org.tnsif.assignment;
+
+public abstract class Bero {
+	
+	 protected String beroType;
+	 protected String beroColour;
+	 protected double price;
+	 
+	 public abstract void calculatePrice();
+	 
+	 public Bero(String beroType, String beroColour) {
+			super();
+			this.beroType = beroType;
+			this.beroColour = beroColour;
+		}
+	 
+	public String getBeroType() {
+		return beroType;
+	}
+	public void setBeroType(String beroType) {
+		this.beroType = beroType;
+	}
+	public String getBeroColour() {
+		return beroColour;
+	}
+	public void setBeroColour(String beroColour) {
+		this.beroColour = beroColour;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+}
+	 
